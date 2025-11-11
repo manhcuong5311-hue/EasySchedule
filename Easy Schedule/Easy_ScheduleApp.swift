ce65fbb52db7c9e1b70b2f66f6eee6ca48716ac5
SamCorp <Manhcuong5311@gmail.com>
2025-11-11 22:23:36 +0700

    Initial Commit

---
+++ Easy Schedule/Easy_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_ScheduleApp.swift
+//  Easy Schedule
+//
+//  Created by Sam Manh Cuong on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Easy_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
